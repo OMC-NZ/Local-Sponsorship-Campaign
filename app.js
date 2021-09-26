31cf3972074c3d7a3f1fa919b052827c5b625fc0
qmqd1225 <qmqd1225@gmail.com>
2021-09-26 23:49:37 +1300

    update first version

---
+++ app.js
@@ -0,0 +1,10 @@
+$(function(){       
+    if($(window).width() > 767){
+        $('#opw-mobile').hide();
+        $('#opw-desktop').show();
+        $(".content-info-words p").attr('style', 'width:' + $(".content-info img").width()+'px');          
+    } else {
+        $('#opw-desktop').hide();
+        $('#opw-opw-mobile').show();
+    }
+})

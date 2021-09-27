9c1ee20303c2c9a69dbf6fa58526be7602bf1314
qmqd1225 <qmqd1225@gmail.com>
2021-09-27 13:36:32 +1300

    update a jquery

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
-$(function(){       
-    if($(window).width() > 767){
-        $('#opw-mobile').hide();
-        $('#opw-desktop').show();
-        $(".content-info-words p").attr('style', 'width:' + $(".content-info img").width()+'px');          
-    } else {
-        $('#opw-desktop').hide();
-        $('#opw-opw-mobile').show();
-    }
-})
+$(function () {
+  if ($(window).width() > 767) {
+    $("#opw-mobile").hide();
+    $("#opw-desktop").show();
+    $(".content-info-words p").attr(
+      "style",
+      "width:" + $(".content-info img").width() + "px"
+    );
+  } else {
+    $("#opw-desktop").hide();
+    $("#opw-opw-mobile").show();
+  }
+  $(window).resize(function () {
+    $(".content-info-words p").attr(
+      "style",
+      "width:" + $(".content-info img").width() + "px"
+    );
+  });
+});
